9b360fb6eeea774c9f829402487b65244ff234ef
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-07-17 11:17:47 +0530

    add parameter Constuctor meth..

---
+++ 17.07.2026/2nd/MainStudent.java
@@ -0,0 +1,10 @@
+class MainStudent{
+	public static void main(String args[]){
+		
+		Student s= new Student(1010,"Tamil");
+		
+		s.getStudent();
+		
+		
+	}
+}
---
+++ 17.07.2026/2nd/Student.java
@@ -0,0 +1,15 @@
+class Student{
+	int id; 
+	String name;
+	
+	Student(int id,String name){
+		this.id=id;
+		this.name=name;
+	}
+
+	void getStudent(){
+		System.out.println("ID :"+id);
+		System.out.println("Name :"+name);
+	
+	}
+}
