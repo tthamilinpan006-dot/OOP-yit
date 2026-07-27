73f000233f66764ca07731c18aaea7dda2755792
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-07-27 14:48:57 +0530

    Inheritance

---
+++ 27.07.2026/Inheritance-1/MainAB.java
@@ -0,0 +1,8 @@
+class MainAB{
+	public static void main(String args[]){
+		A Obja =new A();
+		Obja.getx();
+		B Objb = new B();
+		Objb.gety();
+	}
+}
---
+++ 27.07.2026/Inheritance-2/MainAB.java
@@ -0,0 +1,8 @@
+class MainAB{
+	public static void main(String args[]){
+		//A Obja =new A();
+		B Objb =new B();
+		Objb.getx();
+		Objb.gety();
+	}
+}
